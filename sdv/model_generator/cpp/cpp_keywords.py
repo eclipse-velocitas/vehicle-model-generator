7844229b6986ccc06122216d2061e2859377adbc
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2023-02-14 12:13:20 +0100

    Fix comment for cpp_keywords.py

--- sdv/model_generator/cpp/cpp_keywords.py
+++ sdv/model_generator/cpp/cpp_keywords.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 Robert Bosch GmbH and Microsoft Corporation
+# Copyright (c) 2023 Robert Bosch GmbH and Microsoft Corporation
 #
 # This program and the accompanying materials are made available under the
 # terms of the Apache License, Version 2.0 which is available at
@@ -12,7 +12,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""VehicleModelCppGenerator."""
+"""Reserved keywords of C++ language up to C++20."""
 
 cpp_keywords = [
     "alignas",
