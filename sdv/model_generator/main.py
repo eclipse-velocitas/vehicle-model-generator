c489a7322c773c205895b1482996f213c07c96be
Mladen Milutinovic <mml5bg@bosch.com>
2023-07-13 14:24:30 +0200

    Improvement: Function for model generation

--- sdv/model_generator/main.py
+++ sdv/model_generator/main.py
@@ -30,6 +30,73 @@ from sdv.model_generator.tree_generator.file_import import (
 )
 
 
+def generate_model(
+    language: str,
+    input_file_path: str,
+    name: str = "vehicle",
+    include_dir: list[str] = [],
+    target_folder: str = "./gen_model",
+    strict: bool = True,
+    overlays: list[str] = [],
+    extended_attributes: str = "",
+):
+    """
+    Generate model using the passed arguments.
+
+    Args:
+        language (str): The target language of the generated code,
+        e.g. [python, cpp],
+        input_file_path (str): The file to convert. Currently supports JSON
+        and Vspec file formats,
+        include_dir (list[str]): Add include directory to search for included
+        vspec files,
+        target_folder str: The folder name (with relative path) where the code
+        will be generated,
+        strict (bool): Use strict checking or not,
+        overlays (list[str]): Add overlays that will be layered on top of
+        the VSS file in the order they,
+        extended-attributes (str): Whitelisted extended attributes
+        as comma separated list
+    """
+    include_dirs = ["."]
+    include_dirs.extend(include_dir)
+
+    ext_attributes_list = extended_attributes.split(",")
+    if len(ext_attributes_list) > 0:
+        vspec.model.vsstree.VSSNode.whitelisted_extended_attributes = (
+            ext_attributes_list
+        )
+        print(f"Known extended attributes: {', '.join(ext_attributes_list)}")
+
+    try:
+        tree = FileImport(input_file_path, include_dirs, strict, overlays).load_tree()
+
+        if language == "python":
+            print("Recursing tree and creating Python code...")
+            VehicleModelPythonGenerator(
+                tree,
+                target_folder,
+                name,
+            ).generate()
+            print("All done.")
+        elif language == "cpp":
+            print("Recursing tree and creating c++ code...")
+            VehicleModelCppGenerator(
+                tree,
+                target_folder,
+                name,
+            ).generate()
+            print("All done.")
+        else:
+            print(f"Language {language} is not supported yet.")
+    except vspec.VSpecError as e:
+        print(f"Error: {e}")
+        sys.exit(255)
+    except UnsupportedFileFormat as e:
+        print(f"Error: {e}")
+        sys.exit(255)
+
+
 def main():
     # The arguments we accept
 
@@ -102,49 +169,16 @@ def main():
 
     args = parser.parse_args()
 
-    strict = args.strict
-
-    include_dirs = ["."]
-    include_dirs.extend(args.include_dir)
-
-    # yaml_out = open(args.yaml_file, "w", encoding="utf-8")
-
-    ext_attributes_list = args.extended_attributes.split(",")
-    if len(ext_attributes_list) > 0:
-        vspec.model.vsstree.VSSNode.whitelisted_extended_attributes = (
-            ext_attributes_list
-        )
-        print(f"Known extended attributes: {', '.join(ext_attributes_list)}")
-
-    try:
-        tree = FileImport(
-            args.input_file_path, include_dirs, strict, args.overlays
-        ).load_tree()
-
-        if args.language == "python":
-            print("Recursing tree and creating Python code...")
-            VehicleModelPythonGenerator(
-                tree,
-                args.target_folder,
-                args.name,
-            ).generate()
-            print("All done.")
-        elif args.language == "cpp":
-            print("Recursing tree and creating c++ code...")
-            VehicleModelCppGenerator(
-                tree,
-                args.target_folder,
-                args.name,
-            ).generate()
-            print("All done.")
-        else:
-            print(f"Language {args.language} is not supported yet.")
-    except vspec.VSpecError as e:
-        print(f"Error: {e}")
-        sys.exit(255)
-    except UnsupportedFileFormat as e:
-        print(f"Error: {e}")
-        sys.exit(255)
+    generate_model(
+        args.language,
+        args.input_file_path,
+        args.name,
+        args.include_dir,
+        args.target_folder,
+        args.strict,
+        args.overlays,
+        args.extended_attributes,
+    )
 
 
 if __name__ == "__main__":
