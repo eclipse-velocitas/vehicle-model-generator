861006de4de28f7b04d8111161d0b1d3d450a62d
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2023-08-25 15:18:03 +0200

    Fix ordering and linters

--- sdv/model_generator/tree_generator/file_formats.py
+++ sdv/model_generator/tree_generator/file_formats.py
@@ -13,6 +13,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import json
+import re
 from abc import abstractmethod
 from typing import List
 
@@ -23,6 +24,10 @@ from sdv.model_generator.tree_generator.constants import JSON, VSPEC
 
 # supported file formats
 formats = [VSPEC, JSON]
+
+
+class ExpansionMismatch(Exception):
+    pass
 
 
 class FileFormat:
@@ -73,17 +78,140 @@ class Json(FileFormat):
 
     # VSS nodes have a field "$file_name",
     # so it needs to be added for the vss-tools to work
-    def __extend_fields(self, d: dict):
-        if "children" in d:
-            for child_d in d["children"].values():
-                self.__extend_fields(child_d)
-        d["$file_name$"] = ""
+    def __extend_fields(self, node: dict):
+        if "children" in node:
+            for child in node["children"].values():
+                self.__extend_fields(child)
+        node["$file_name$"] = ""
         return
+
+    # Do a simple comparison of children names for now
+    def __match(self, d1: dict, d2: dict):
+        return d1.keys() == d2.keys()
+
+    def __is_expanded(self, d: dict):
+        return "expanded" in d and d["expanded"]
+
+    def __to_instance_description(self, instances):
+        assert instances
+
+        common_prefix = None
+        indices = set()
+        pattern = re.compile("([^0-9]+)([0-9]+)")
+        for instance in instances:
+            match = pattern.fullmatch(instance)
+            if not match:
+                return instances
+            (prefix, index) = match.groups()
+            if common_prefix is None:
+                common_prefix = prefix
+            elif prefix != common_prefix:
+                return instances
+            indices.add(int(index))
+
+        min_index = None
+        max_index = None
+        for index in indices:
+            if min_index is None:
+                min_index = index  # sets are sorted
+                max_index = index
+            # make sure every index between min and (final) max is there
+            elif index == max_index + 1:
+                max_index = index
+            else:
+                return instances
+
+        return f"{common_prefix}[{min_index},{max_index}]"
+
+    def __generate_instance_list(self, instances):
+        assert len(instances) > 0
+
+        if len(instances) == 1:
+            return self.__to_instance_description(instances[0])
+
+        instance_list = []
+        for instance_set in instances:
+            instance_list.append(self.__to_instance_description(instance_set))
+        return instance_list
+
+    def __get_common_archetype_of_children(self, node: dict):
+        common_archetype = None
+        instances: list[list] = []
+
+        if "children" in node:
+            this_instances = []
+            for child_name, child_attrs in node["children"].items():
+                (archetype, sub_instances) = self.__collapse_instances(child_attrs)
+                if archetype:
+                    if common_archetype is None:
+                        common_archetype = archetype
+                        archetype_giver = child_name
+                    elif not self.__match(archetype, common_archetype):
+                        raise ExpansionMismatch(
+                            f"Nodes '{archetype_giver}' and '{child_name}' "
+                            "contain diverging expanded children"
+                        )
+                    if sub_instances:
+                        if not instances:
+                            instances = sub_instances
+                        elif sub_instances != instances:
+                            raise ExpansionMismatch(
+                                f"Nodes '{archetype_giver}' and '{child_name}' "
+                                "have diverging nested instances:\n"
+                                f"{instances} and {sub_instances}"
+                            )
+                    elif instances:
+                        raise ExpansionMismatch(
+                            f"Nodes '{archetype_giver}' and '{child_name}' "
+                            "have diverging nested instances:\n"
+                            f"{instances} and {sub_instances}"
+                        )
+                    this_instances.append(child_name)
+            if this_instances:
+                instances.insert(0, this_instances)
+
+        return (common_archetype, instances)
+
+    def __collapse_instances(self, node: dict):
+        (common_archetype, instances) = self.__get_common_archetype_of_children(node)
+
+        if self.__is_expanded(node):
+            if not common_archetype:
+                common_archetype = node["children"]
+            return (common_archetype, instances)
+
+        if common_archetype:
+            # Remove expanded children
+            for child_name in instances[0]:
+                del node["children"][child_name]
+
+            if self.__is_expanded(node):
+                # if this layer is also an expanded one, in the layer below
+                # no children must be left
+                if node["children"]:
+                    raise ExpansionMismatch(
+                        f'Node "{child_name}" has children other than instances:\n'
+                        + node["children"]
+                    )
+            else:
+                # if this layer is not an expanded one, the children of the
+                # common_archetype needs to become children of this layer ...
+                node["children"] |= common_archetype
+
+                # ... and we need to genrate the list of instances
+                node["instances"] = self.__generate_instance_list(instances)
+
+                common_archetype = None
+                instances = []
+
+        return (common_archetype, instances)
 
     def load_tree(self):
         print("Loading json...")
         output_json = json.load(open(self.file_path))
-        self.__extend_fields(next(iter(output_json.values())))
+        print("Collapsing expanded instances...")
+        self.__collapse_instances(next(iter(output_json.values())))
         print("Generating tree from json...")
+        self.__extend_fields(next(iter(output_json.values())))
         tree = vspec.render_tree(output_json)
         return tree
